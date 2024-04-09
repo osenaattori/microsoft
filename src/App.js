7cd8bb974fa08693b3cc41ccb50e306e64051296
Ose <37250900+osenaattori@users.noreply.github.com>
2024-04-09 15:22:44 +0300

    hhh

--- src/App.js
+++ src/App.js
@@ -1,22 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Kaikki Microsofin Admin linkit</p>
+        <ul className="admin-links">
+          <li>
+            <a
+              href="https://aad.portal.azure.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Azure Active Directory Admin Center
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://endpoint.microsoft.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Microsoft Intune Admin Center
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://admin.microsoft.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Microsoft 365 Admin Center
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://compliance.microsoft.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Microsoft Purview
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://account.azure.com/Subscriptions"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Azure Subscriptions
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://securitycopilot.microsoft.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Microsoft Security Copilot
+            </a>
+          </li>
+          <li>
+            <a
+              href="Microsoft Security Copilot"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Microsoft Defender For Endpoint
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://admin.teams.microsoft.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Microsoft Teams Admin
+            </a>
+          </li>
+        </ul>
       </header>
     </div>
   );
